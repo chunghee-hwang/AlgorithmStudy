28e20e0084c1f2520c27262e6ace9593c98c06bc
hch0821 <hch0821@gmail.com>
2019-11-06 22:37:28 +0900

    Update StandinLine.java

--- src/greedy/StandinLine.java
+++ src/greedy/StandinLine.java
@@ -1,19 +1,20 @@
 package greedy;
 
 import java.util.Scanner;
-//https://www.acmicpc.net/problem/1138 
+//https://www.acmicpc.net/problem/1138
 public class StandinLine {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // 사람 수 (키는 1~n)
         int []d = new int[11];
-        for(int i = 1; i <= n; i++){
-            int x =  sc.nextInt();
+        for(int i = 1; i <= n; i++){ //i : 줄세울 사람의 키
+            int x =  sc.nextInt(); //x: 자신보다 키가 더 큰 사람들의 수
             int count = 0;
 
-            for(int j = 1; j <= n; j++){
-                if(count == x && d[j] == 0){
+            for(int j = 1; j <= n; j++){ //j: 줄세울 사람의 위치
+                if(count == x && d[j] == 0)
+                {
                     d[j] = i;
                     break;
                 }
