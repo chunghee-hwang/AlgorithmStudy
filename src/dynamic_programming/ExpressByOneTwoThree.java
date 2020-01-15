cfb34ec86905ca7c102a6e11e25c8151a8e9964d
hch0821 <hch0821@gmail.com>
2020-01-15 21:16:58 +0900

    dp 문제 추가

---
+++ src/dynamic_programming/ExpressByOneTwoThree.java
@@ -0,0 +1,25 @@
+package dynamic_programming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//https://www.acmicpc.net/problem/9095
+public class ExpressByOneTwoThree {
+    private static int[] mem = new int[12];
+    private static int dp(int n) {
+        if (mem[n] != 0) return mem[n];
+        if (n == 1 || n == 2) return n;
+        if (n == 3) return 4;
+        return mem[n] = dp(n-1)+dp(n-2)+dp(n-3);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            System.out.println(dp(Integer.parseInt(br.readLine())));
+        }
+    }
+
+}
