95d1e0468501f467657bd1d3ac0e5743a2ad42e1
hch0821 <hch0821@gmail.com>
2020-01-19 20:33:11 +0900

    Update GetCandy.java

--- src/dynamic_programming/GetCandy.java
+++ src/dynamic_programming/GetCandy.java
@@ -7,28 +7,33 @@ import java.util.StringTokenizer;
 /*
 https://codeup.kr/problem.php?id=3703
  */
+
 public class GetCandy {
-    private static int[][] map;
+    private static int[][] map; //맵
+
+    //mem[y][x]: (y,x)셀부터 마지막 셀까지의 경로중 사탕을 가장 많이 얻을 수 있는 경로의 사탕개수합
     private static int[][] mem;
     private static int n, m;
     private static int dp(int y, int x) {
-        if (mem[y][x] != 0)
-            return mem[y][x];
+        //이미 (y,x)셀부터 마지막 셀까지의 경로중 사탕을 가장 많이 얻을 수 있는 경로의 사탕개수합을 알고 있다면 반환
+        if (mem[y][x] != 0) return mem[y][x];
         boolean yCheck = y + 1 < n;
         boolean xCheck = x + 1 < m;
         int down = -1;
         int right = -1;
-        if (yCheck) {
-            down = map[y][x]+dp(y + 1, x);
-        }
-        if (xCheck) {
-            right = map[y][x]+dp(y, x + 1);
-        }
-        if (!yCheck && !xCheck)
-        {
-            return mem[y][x] = map[y][x];
-        }
-        return mem[y][x] = Math.max(down, right);
+        /*짧은 길로 가기 위해선 무조건 오른쪽이나 아래로만 가야함*/
+
+        //바로 아래 셀 부터 마지막 셀 경로들의 가장 많이 모을 수 있는 사탕의 수
+        if (yCheck) down = dp(y + 1, x);
+
+        //바로 오른쪽 셀 부터 마지막 셀 경로들의 가장 많이 모을 수 있는 사탕의 수
+        if (xCheck) right = dp(y, x + 1);
+
+        //마지막 셀을 방문할 경우 그 셀의 사탕 수 반환
+        if (!yCheck && !xCheck) return mem[y][x] = map[y][x];
+
+        //현재 위치의 사탕수 + max(아래셀~마지막셀 까지의 최대 모을 수 있는 사탕, 오른쪽셀~마지막셀까지의 최대 모을 수 있는 사탕) 저장
+        return mem[y][x] = map[y][x]+Math.max(down, right);
     }
 
     public static void main(String[] args) throws IOException {
