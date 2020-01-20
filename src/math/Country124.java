b02a346d92a9d038c6d8218a5f71b2cc692b6d66
hch0821 <hch0821@gmail.com>
2020-01-20 21:17:26 +0900

    Create Country124.java

---
+++ src/math/Country124.java
@@ -0,0 +1,22 @@
+package math;
+
+public class Country124 {
+    //https://programmers.co.kr/learn/courses/30/lessons/12899
+    public static String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        int[] map = {4,1,2};
+        int share = n; // 몫
+        int rest;
+        while(share != 0){
+            rest = share%3;
+            share/=3;
+            if(rest == 0) share-=1;
+            sb.insert(0, map[rest]);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(13));
+    }
+}
