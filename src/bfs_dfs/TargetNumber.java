ab2c0b7d9e36402abd22232c2d45ee4940ebe6e2
hch0821 <hch0821@gmail.com>
2020-01-25 22:40:18 +0900

    DP, BFS문제 추가

---
+++ src/bfs_dfs/TargetNumber.java
@@ -0,0 +1,48 @@
+package bfs_dfs;
+
+import java.util.ArrayDeque;
+
+public class TargetNumber {
+    private static class Data{
+        int idx;
+        int sign;
+        int sum;
+        private Data(int idx, int sign, int sum) {
+            this.idx = idx;
+            this.sign = sign;
+            this.sum = sum;
+        }
+        static Data make(int i, int s, int s2){
+            return new Data(i,s, s2);
+        }
+    }
+    public int solution(int[] numbers, int target) {
+        int cnt = 0;
+        int n = numbers.length;
+        ArrayDeque<Data> q = new ArrayDeque<>();
+        q.add(Data.make(0,1, 0));
+        q.add(Data.make(0,-1, 0));
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            for(int k=0; k <size; k++){
+                Data data = q.remove();
+                int idx = data.idx;
+                int sum = data.sum;
+                sum += (numbers[idx] * data.sign);
+                if(idx == n-1){
+                    if(sum == target) cnt++;
+                }else{
+                    q.add(Data.make(idx + 1, 1, sum));
+                    q.add(Data.make(idx + 1, -1, sum));
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 1, 1, 1, 1};
+        System.out.println(new TargetNumber().solution(numbers, 3));
+    }
+}
---
+++ src/dynamic_programming/TwoByNTiling.java
@@ -0,0 +1,17 @@
+package dynamic_programming;
+
+public class TwoByNTiling {
+    private static int[] mem;
+    private static int dp(int n){
+        if(mem[n] != 0) return mem[n];
+        if(n == 1 || n == 2)return n;
+        return mem[n] = (dp(n-1) + dp(n-2))%1000000007;
+    }
+    public static int solution(int n) {
+        mem = new int[n+1];
+        return dp(n);
+    }
+    public static void main(String[] args) {
+        int n = 4;
+    }
+}
