48ca88a0ee38e004ce60fce6c9c0c2b6e4b7de0d
hch0821 <hch0821@gmail.com>
2020-01-28 21:59:24 +0900

    Update Solution3.java

--- src/coding_test/쏘카/Solution3.java
+++ src/coding_test/쏘카/Solution3.java
@@ -6,8 +6,7 @@ public class Solution3
     private boolean[] visit;
     private final int BIG_NUMBER = 100001;
     private int toNext(int idx, int cnt, int n){
-        if(idx<0 || idx>=n) return BIG_NUMBER;
-        if(visit[idx]) return BIG_NUMBER;
+        if(idx<0 || idx>=n || visit[idx]) return BIG_NUMBER;
         visit[idx] = true;
         cnt++;
         if(idx == n-1) return cnt;
