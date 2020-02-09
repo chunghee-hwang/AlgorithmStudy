0e037bf166bd6ce0e9fedda1211a0daff3f211fc
hch0821 <hch0821@gmail.com>
2020-02-09 19:45:09 +0900

    Update Eratos.java

--- src/math/Eratos.java
+++ src/math/Eratos.java
@@ -1,48 +1,39 @@
 package math;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+// https://ko.wikipedia.org/wiki/%EC%97%90%EB%9D%BC%ED%86%A0%EC%8A%A4%ED%85%8C%EB%84%A4%EC%8A%A4%EC%9D%98_%EC%B2%B4
 public class Eratos {
-    public static void main(String[] args) {
-        // ArrayList로 구현
-        ArrayList<Boolean> primeList;
-
+    public static void main(String[] args) throws IOException {
+        boolean[] primes;
         // 사용자로부터의 콘솔 입력
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        primes = new boolean[n+1]; // n+1만큼 할당
         // n <= 1 일 때 종료
         if(n <= 1) return;
 
-        // n+1만큼 할당
-        primeList = new ArrayList<Boolean>(n+1);
-        // 0번째와 1번째를 소수 아님으로 처리
-        primeList.add(false);
-        primeList.add(false);
         // 2~ n 까지 소수로 설정
         for(int i=2; i<=n; i++ )
-            primeList.add(i, true);
+            primes[i] = true;
 
         // 2 부터  ~ i*i <= n
         // 각각의 배수들을 지워간다.
         for(int i=2; (i*i)<=n; i++){
-            if(primeList.get(i)){
-                for(int j = i*2; j<=n; j+=i) primeList.set(j, false);
-                //i*i 미만은 이미 처리되었으므로 j의 시작값은 i*i로 최적화할 수 있다.
-            }
+            if(primes[i])
+                for(int j = i*i; j<=n;j+=i)
+                    primes[j] = false;
         }
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         sb.append("{");
         for(int i=0; i<=n; i++){
-            if(primeList.get(i)){
+            if(primes[i]){
                 sb.append(i);
-                sb.append(",");
+                sb.append(", ");
             }
         }
-        sb.setCharAt(sb.length()-1, '}');
-
+        sb.setCharAt(sb.length()-2, '}');
         System.out.println(sb.toString());
-
     }
 }
