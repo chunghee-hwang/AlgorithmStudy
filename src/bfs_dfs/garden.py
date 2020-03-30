9f59452293e8a89574be880b5ee13690d6319f46
hch0821 <hch0821@gmail.com>
2020-03-30 21:36:13 +0900

    Update garden.py
    최적화1

--- src/bfs_dfs/garden.py
+++ src/bfs_dfs/garden.py
@@ -2,59 +2,62 @@
 from sys import stdin
 from collections import deque
 
-color_green = 3
-color_red = 4
+water = '0'
+color_green = '3'
+color_red = '4'
 len_row = 0
 len_column = 0
 garden = []
 liquid_land_positions = deque()  # (y, x)
+len_liquid_land_positions = 0
+flower_count = 0
 max_flower_count = 0
+time = 0
+
+def read_ints():
+    return list(map(int, read_line().split()))
 
 
 def read_line():
     return stdin.readline()
 
 
-def read_ints():
-    return list(map(int, read_line().split()))
-
-
-def init_garden(n):
+def init_garden():
     global garden
-    garden = [read_ints() for _ in range(n)]
+    for y in range(len_row):
+        line = read_line()
+        for x in range(0, len_column*2, 2):
+            if line[x] == '2':
+                liquid_land_positions.append((y, x//2))
+        garden.append(line)
 
 
-def find_liquid_land_positions(n, m):
-    for y in range(n):
-        for x in range(m):
-            if garden[y][x] == 2:
-                liquid_land_positions.append((y, x))
-
-
-def put_liquid(n, green, red, green_positions, red_positions, idx):
-    global max_flower_count
+def put_liquid(green, red, green_positions, red_positions, idx):
+    global max_flower_count, flower_count
     if green == 0 and red == 0:
-        print('==')
-        max_flower_count = max(simulate_garden(green_positions, red_positions), max_flower_count)
+        simulate_garden(green_positions, red_positions)
+        max_flower_count = max(flower_count, max_flower_count)
+        flower_count = 0
         return
-    for idx2 in range(idx + 1, n):
+    for idx2 in range(idx + 1, len_liquid_land_positions):
         if green - 1 > -1:
             green -= 1
-            green_positions.append(idx2)
-            put_liquid(n, green, red, green_positions, red_positions, idx2)
+            green_positions.append(liquid_land_positions[idx2])
+            put_liquid(green, red, green_positions, red_positions, idx2)
             green_positions.pop()
             green += 1
         if red - 1 > -1:
             red -= 1
-            red_positions.append(idx2)
-            put_liquid(n, green, red, green_positions, red_positions, idx2)
+            red_positions.append(liquid_land_positions[idx2])
+            put_liquid(green, red, green_positions, red_positions, idx2)
             red_positions.pop()
             red += 1
 
 
-def visit_land(time, q, y, x, liquid_color, visit, flower_positions):
-    water = 0
-    if garden[y][x] == water or (y, x) in flower_positions:
+def visit_land(q, y, x, liquid_color, visit, flower_exists):
+    global flower_count
+
+    if garden[y][x*2] == water or flower_exists[y][x]:
         return
     if liquid_color == color_red:
         other_color = color_green
@@ -64,71 +67,57 @@ def visit_land(time, q, y, x, liquid_color, visit, flower_positions):
     visit_color, visit_time = visit_color_time
     if visit_time != -1:
         if visit_time == time and visit_color == other_color:
-            flower_positions.append((y, x))
+            flower_exists[y][x] = True
+            flower_count += 1
     else:
         visit_color_time[0] = liquid_color
         visit_color_time[1] = time
         q.append((y, x))
 
 
-def spread_liquid(time, q, liquid_color, visit, flower_positions):
+def spread_liquid(q, liquid_color, visit, flower_exists):
     y, x = q.popleft()
-    if (y, x) in flower_positions:
+    if flower_exists[y][x]:
         return
-
-    new_y = y - 1
-    new_x = x
-    if new_y > -1:
-        visit_land(time, q, new_y, new_x, liquid_color, visit, flower_positions)
-    new_y = y
-    new_x = x - 1
-    if new_x > -1:
-        visit_land(time, q, new_y, new_x, liquid_color, visit, flower_positions)
-    new_y = y + 1
-    new_x = x
-    if new_y < len_row:
-        visit_land(time, q, new_y, new_x, liquid_color, visit, flower_positions)
-    new_y = y
-    new_x = x + 1
-    if new_x < len_column:
-        visit_land(time, q, new_y, new_x, liquid_color, visit, flower_positions)
+    if y-1 > -1:
+        visit_land(q, y-1, x, liquid_color, visit, flower_exists)
+    if x-1 > -1:
+        visit_land(q, y, x-1, liquid_color, visit, flower_exists)
+    if y+1 < len_row:
+        visit_land(q, y+1, x, liquid_color, visit, flower_exists)
+    if x+1 < len_column:
+        visit_land(q, y, x+1, liquid_color, visit, flower_exists)
 
 
 def simulate_garden(green_positions, red_positions):
+    global time
     liquid_visit = [[[-1, -1] for _ in range(len_column)] for _ in range(len_row)]  # [color, time]
     green_q = deque()
     red_q = deque()
-    time = 0
-    flower_positions = []
+    flower_exists = [[False for _ in range(len_column)] for _ in range(len_row)]
     for green_position in green_positions:
-        positions = liquid_land_positions[green_position]
-        visit_color_time = liquid_visit[positions[0]][positions[1]]
+        visit_color_time = liquid_visit[green_position[0]][green_position[1]]
         visit_color_time[0] = color_green
         visit_color_time[1] = time
-        green_q.append(positions)
+        green_q.append(green_position)
     for red_position in red_positions:
-        positions = liquid_land_positions[red_position]
-        visit_color_time = liquid_visit[positions[0]][positions[1]]
+        visit_color_time = liquid_visit[red_position[0]][red_position[1]]
         visit_color_time[0] = color_red
         visit_color_time[1] = time
-        red_q.append(positions)
+        red_q.append(red_position)
     while green_q and red_q:
-        # print('red_q: {}\ngreen_q: {}\n'.format(red_q, green_q))
         green_q_size = len(green_q)
         red_q_size = len(red_q)
         time += 1
         for _ in range(green_q_size):
-            spread_liquid(time, green_q, color_green, liquid_visit, flower_positions)
+            spread_liquid(green_q, color_green, liquid_visit, flower_exists)
         for _ in range(red_q_size):
-            spread_liquid(time, red_q, color_red, liquid_visit, flower_positions)
-    flower_created_cnt = len(flower_positions)
-    print('flower_created_cnt:', flower_created_cnt)
-    return flower_created_cnt
+            spread_liquid(red_q, color_red, liquid_visit, flower_exists)
 
 
 if __name__ == '__main__':
     len_row, len_column, len_green, len_red = read_ints()
-    init_garden(len_row)
-    find_liquid_land_positions(len_row, len_column)
-    put_liquid(len(liquid_land_positions), len_green, len_red, deque(), deque(), -1)
-    print('max_flower_count:', max_flower_count)
+    init_garden()
+    len_liquid_land_positions = len(liquid_land_positions)
+    put_liquid(len_green, len_red, deque(), deque(), -1)
+    print(max_flower_count)
