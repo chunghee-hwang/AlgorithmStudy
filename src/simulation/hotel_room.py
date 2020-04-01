674615dacf11cd82d814b475847feaf08fe381bb
hch0821 <hch0821@gmail.com>
2020-04-01 22:59:28 +0900

    Update hotel_room.py

--- src/simulation/hotel_room.py
+++ src/simulation/hotel_room.py
@@ -1,20 +1,23 @@
 # https://programmers.co.kr/learn/courses/30/lessons/64063?language=cpp
+from collections import defaultdict
+
+
 def solution(k, room_number):
-    next_empty = list(range(k + 1))
+    next_empty = defaultdict(int)
     ans_idx = 0
-    n = len(room_number)
     for num in room_number:
-        prev_room_num = room_number[ans_idx] = next_empty[num]
-        if prev_room_num != k:
-            new_room_num = next_empty[prev_room_num + 1]
-            num2 = prev_room_num
-            if ans_idx != n - 1:
-                while 0 < num2 and prev_room_num == next_empty[num2]:
-                    next_empty[num2] = new_room_num
-                    num2 -= 1
+        tmp_num = num
+        while next_empty[tmp_num] != 0:
+            tmp_num = next_empty[tmp_num]
+        room_number[ans_idx] = tmp_num
+        if tmp_num < k:
+            nex = next_empty[tmp_num + 1]
+            if nex == 0:
+                nex = tmp_num+1
+            next_empty[tmp_num] = nex
         ans_idx += 1
     return room_number
 
 
 if __name__ == '__main__':
-    print(solution(10, [1,3,4,1,3,1]))
+    print(solution(10, [1, 3, 4, 1, 3, 1]))
