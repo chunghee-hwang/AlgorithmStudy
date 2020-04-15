c5913faffff26fb596fb94e4a784454cdfc485f4
Chunghee Hwang <hch0821@gmail.com>
2020-04-15 23:23:15 +0900

    Update connect_island(kruskal).py

--- src/greedy/connect_island(kruskal).py
+++ src/greedy/connect_island(kruskal).py
@@ -1,3 +1,4 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42861
 parent = []
 level = []
 
@@ -35,23 +36,29 @@ def union(u, v):
 # 최소 신장 트리 만들기
 def make_min_spanning_tree(costs, n):
     # mst = []
-    mst_weight_sum = 0 # MST 가중치 누적 합
-    mst_edge_count = 0 # MST 간선 개수
+    mst_weight_sum = 0  # MST 가중치 누적 합
+    mst_edge_count = 0  # MST 간선 개수
     init_set(costs)
-    costs.sort(key=lambda edge: edge[2]) # 그래프의 간선을 가중치를 기준으로 정렬
+    costs.sort(key=lambda edge: edge[2])  # 그래프의 간선을 가중치를 기준으로 정렬
     for edge in costs:  # 가중치가 적은 순으로 정렬된 간선을 순회
         v1, v2, weight = edge
         if find(v1) != find(v2):  # 사이클이 안 생기면(v1과 v2가 같은 집합이 아니라면)
             union(v1, v2)  # 새로운 간선을 같은 집합으로 만들기
             # mst.append(edge)  # 최소 신장 트리에 추가
-            mst_weight_sum+=weight
-            mst_edge_count+=1
-        if mst_edge_count == n - 1: # 최소 신장 트리의 간선 개수가 n-1개가 되면 완료 처리
+            mst_weight_sum += weight
+            mst_edge_count += 1
+        if mst_edge_count == n - 1:  # 최소 신장 트리의 간선 개수가 n-1개가 되면 완료 처리
             break
     return mst_weight_sum
+
+
 def solution(n, costs):
     global parent, level
     parent = [0] * n
     level = [1] * n
     answer = make_min_spanning_tree(costs, n)
     return answer
+
+
+if __name__ == '__main__':
+    print(solution(4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]))
