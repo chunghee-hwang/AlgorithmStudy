30989b3b319c95aaa948af767b409acc2e8c7a2b
꿈많은개발자 <hch0821@naver.com>
2021-01-23 01:22:59 +0900

    Update: 외벽점검.py - 주석 추가

--- src/kakao/level3/외벽점검.py
+++ src/kakao/level3/외벽점검.py
@@ -1,26 +1,37 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60062
 from itertools import permutations
+
+
 def solution(n, weak, dist):
-    weak_count = len(weak)
+    length = len(weak)
     friend_count = len(dist)
-    answer = friend_count+1
-    for i in range(weak_count):
+    answer = friend_count+1  # 투입할 친구 수의 최솟값을 찾아야 하므로 len(dist) + 1로 초기화
+    # 길이를 2배로 늘려서 원형을 일자 형태로 변형하기
+    for i in range(length):
         weak.append(weak[i]+n)
-    print(weak)
+
+    # 친구를 나열하는 모든 경우 구하기
     friends_perm = list(permutations(dist, friend_count))
-    for idx in range(weak_count+1):
+
+    # 0 부터 length - 1까지의 위치를 각각 시작점으로 설정
+    for start in range(length):
+        # 친구를 나열하는 모든 경우 각각에 대하여 확인
         for friends in friends_perm:
-            fcount = 1
-            flimit = friends[fcount-1]+weak[idx]
-            for idx2 in range(idx, idx+weak_count):
-                if flimit < weak[idx2]:
-                    fcount+=1
-                    if fcount > friend_count:
+            count = 1  # 투입할 친구의 수
+            # 해당 친구가 점검할 수 있는 마지막 위치
+            position = weak[start]+friends[count-1]
+            # 시작점부터 모든 취약한 지점을 확인
+            for index in range(start, start+length):
+                # 점검할 수 있는 위치를 벗어나는 경우
+                if position < weak[index]:
+                    count += 1  # 새로운 친구를 투입
+                    if count > friend_count:  # 더 투입이 불가능하다면 종료
                         break
-                    flimit = friends[fcount-1]+weak[idx2]
-        answer = min(answer, fcount)
+                    position = weak[index]+friends[count-1]
+            answer = min(answer, count)  # 최솟값 계산
     if answer > friend_count:
         return -1
     return answer
+
 
 solution(12, [1, 5, 6, 10], [1, 2, 3, 4])
