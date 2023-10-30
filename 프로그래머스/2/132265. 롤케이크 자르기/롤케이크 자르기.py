0ed1bc0427078eb8e9b4e09b36b9d337c98fa4fc
Chunghee Hwang <hch0821@gmail.com>
2023-10-30 15:44:50 +0900

    [level 2] Title: 롤케이크 자르기, Time: 808.53 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,18 +1,14 @@
 from collections import Counter
 def solution(topping):
     answer = 0
-    left = Counter()
-    leftKeysSize = 0
+    left = set()
     right = Counter(topping)
-    rightKeysSize = len(right.keys())
     for idx, t in enumerate(topping):
-        left[t]+=1
-        if left[t] == 1:
-            leftKeysSize +=1
+        left.add(t)
         right[t]-=1
         if right[t] == 0:
-            rightKeysSize-=1
-        if leftKeysSize == rightKeysSize:
+            right.pop(t)
+        if len(left) == len(right):
             answer+=1
         
     return answer
